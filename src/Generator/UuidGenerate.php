4c33ada53a7d4b51a0dbef833ec500a4e086fa2f
Firdovsi Mamedov <firdovsi.mamedov@gmail.com>
2023-09-23 13:57:37 +0400

    broke code style

--- src/Generator/UuidGenerate.php
+++ src/Generator/UuidGenerate.php
@@ -1,14 +1,18 @@
 <?php
 
 namespace App\Generator;
+use App\Kernel;
+use App\Generator\MessageGenerator;
 
-class UuidGenerate
-{
+class UuidGenerate{
+
     /**
      * @throws \Exception
      */
-    public function generate(): int
-    {
+
+    public function generate():
+    int {
         return random_int(0, 100);
     }
+
 }
